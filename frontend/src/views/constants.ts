7ae1c95fd502ee14e9c4bdaa5686a103d71b59ea
Bepen <neupanebepen@gmail.com>
2025-04-12 21:16:43 -0400

    some changes to use lf server for node api

--- frontend/src/views/constants.ts
+++ frontend/src/views/constants.ts
@@ -1,1 +1,1 @@
-export const serverHost = 'http://fakeapi.com'; // TODO: need to make this test server in development mode and production server in production mode
+export const serverHost = 'https://leagueforecaster.com/api';
