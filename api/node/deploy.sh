a151955e280fc7ba8a88423235fa3b5bcdaaa79a
Bepen <neupanebepen@gmail.com>
2025-04-13 17:19:22 -0400

    remove certs

--- api/node/deploy.sh
+++ api/node/deploy.sh
@@ -5,6 +5,6 @@ echo "Pulling latest changes from main"
 git pull origin main
 
 echo "Deploying files to the server"
-scp -r src babel.config.cjs jest.config.js package.json setupTests.js yarn.lock ca.crt ca.key cert.crt cert.key lf@192.168.0.113:/home/lf/node-server/
+scp -r src babel.config.cjs jest.config.js package.json setupTests.js yarn.lock lf@192.168.0.113:/home/lf/node-server/
 
 echo "Done"
